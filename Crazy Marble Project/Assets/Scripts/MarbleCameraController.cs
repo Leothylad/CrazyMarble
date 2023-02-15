fd5e6fb623f5a83abb0692846f75ebb063c25033
Leothylad <93407644+Leothylad@users.noreply.github.com>
2023-02-15 13:27:48 -0800

    Day 2 mrble
    Forgot to add textures

---
+++ Crazy Marble Project/Assets/Scripts/MarbleCameraController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarbleCameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _target;
+    [SerializeField]
+    private float _zDistance;
+    [SerializeField]
+    private float _yDistance;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 p = _target.position;
+        p.z = p.z - _zDistance;
+        p.y += _yDistance;
+        transform.position = p;
+    }
+}
